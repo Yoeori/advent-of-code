70ce0e5361bada4f0894a9ede0783689f0bb9a19
Yoeri Otten <yoeori@live.nl>
2020-12-03 06:19:38 +0100

    Add 2020 day 03

---
+++ 2020/src/day03.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Pos(usize, usize);
+
+pub fn main() {
+    let file = fs::read_to_string("puzzles/3.txt").unwrap();
+    let map: Vec<Vec<char>> = file.split("\n").map(|l| l.chars().collect()).collect();
+
+    println!("Answer to exercise 1: {}", check_slope(&map, 1, 3));
+    
+    let mut total = check_slope(&map, 1, 1);
+    total *= check_slope(&map, 1, 3);
+    total *= check_slope(&map, 1, 5);
+    total *= check_slope(&map, 1, 7);
+    total *= check_slope(&map, 2, 1);
+
+    println!("Answer to exercise 2: {}", total);
+}
+
+fn check_slope(map: &Vec<Vec<char>>, slope_y: usize, slope_x: usize) -> usize {
+    let mut treecount = 0;
+    let mut pos = Pos(0, 0);
+
+    while pos.0 < map.len()-slope_y {
+        pos.0 += slope_y;
+        pos.1 += slope_x;
+
+        if map[pos.0][pos.1 % map[pos.0].len()] == '#' {
+            treecount += 1;
+        }
+    }
+
+    treecount
+}
