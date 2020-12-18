47f679f7693e2fb5f2df7f9be442bc19c9fae478
Yoeri Otten <yoeori@live.nl>
2020-12-18 21:32:03 +0100

    Add 2020 day 18

---
+++ 2020/src/day18.rs
@@ -0,0 +1,56 @@
+use std::time::SystemTime;
+
+pub fn main() {
+    let file = include_str!("../puzzles/18.txt")
+        .split('\n')
+        .map(|l| l.split(' ').map(|c| c.chars()).flatten().map(|c| c.to_string()));
+
+    let time = SystemTime::now();
+    println!("Solution to exercise 1: {}", file.clone().map(|mut l| eval(&mut l)).sum::<usize>());
+    println!("Solution to exercise 2: {}", file.clone().map(|mut l| eval_addition(&mut l)).sum::<usize>());
+    println!("{:?}", time.elapsed());
+}
+
+fn eval(seq: &mut dyn Iterator<Item=String>) -> usize {
+    let mut total = num(seq, &eval).unwrap();
+
+    while let Some(operator) = seq.next() {
+        match &operator[..] {
+            "*" => total *= num(seq, &eval).unwrap(),
+            "+" => total += num(seq, &eval).unwrap(),
+            ")" => break,
+            _   => panic!("Invalid operator: {}", operator)
+        }
+    }
+
+    total
+}
+
+/// First evaluates all '+' operators and than evaluates the resulting vec
+fn eval_addition(seq: &mut dyn Iterator<Item=String>) -> usize {
+    let mut res: Vec<String> = vec![];
+
+    let mut prev = num(seq, &eval_addition).unwrap();
+    while let Some(operator) = seq.next() {
+        match &operator[..] {
+            ")" => break,
+            "+" => prev += num(seq, &eval_addition).unwrap(),
+            op => {
+                res.push(prev.to_string());
+                res.push(op.to_string());
+                prev = num(seq, &eval_addition).unwrap()
+            }
+        }
+    }
+
+    res.push(prev.to_string());
+    eval(&mut res.into_iter())
+}
+
+fn num(seq: &mut dyn Iterator<Item=String>, evaluater: &impl Fn(&mut dyn Iterator<Item=String>) -> usize) -> Option<usize> {
+    match seq.next().as_deref() {
+        Some("(") => Some(evaluater(seq)),
+        Some(n) => Some(n.parse().unwrap()),
+        None => None
+    }
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -16,6 +16,7 @@ mod day14;
 mod day15;
 mod day16;
 mod day17;
+mod day18;
 
 use std::env;
 use ansi_term::Style;
@@ -23,7 +24,7 @@ use ansi_term::Style;
 fn main() {
     let days: &[fn()-> ()] = &[day01::main, day02::main, day03::main, day04::main, day05::main, day06::main, day07::main, day08::main,
                                day09::main, day10::main, day11::main, day12::main, day13::main, day14::main, day15::main, day16::main,
-                               day17::main];
+                               day17::main, day18::main];
                                     
     let args: Vec<String> = env::args().collect();
 
