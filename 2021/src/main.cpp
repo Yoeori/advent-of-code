9f5dc974217521443c548ee22a241b18c523e576
Yoeri Otten <yoeri@otten.dev>
2022-08-29 02:14:09 +0200

    Add 2021 C++ base

---
+++ 2021/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef void (*DayFunctions)();
+
+int main(int argc, char *argv[]) {
+    DayFunctions days[] = {};
+
+    if (argc >= 2) {
+        int day;
+        try {
+            day = stoi(argv[1]);
+        } catch (const exception &e) {
+          day = 0;
+        }
+        
+        if (day >= 1 && day <= (sizeof(days) / sizeof(days[0]))) {
+            days[day - 1]();
+        } else {
+            cout << "No valid day was given\n";
+            return 1;
+        }
+    } else {
+        days[(sizeof(days) / sizeof(days[0])) - 1]();
+    }    
+
+    return 0;
+}
