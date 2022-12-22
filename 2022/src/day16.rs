503484bab70ead21cf310f8912c3d4d4a382cb61
Yoeri Otten <yoeri@otten.dev>
2022-12-22 17:00:38 +0100

    Improve 2022 day 16

--- 2022/src/day16.rs
+++ 2022/src/day16.rs
@@ -1,75 +1,70 @@
-use std::{collections::{HashMap, HashSet}, cmp};
+use std::{collections::HashMap, cmp};
 
 #[derive(Default, Debug, Clone)]
-struct Valve<T> {
-    name: T,
+struct Valve {
     flow_rate: i32,
-    dist: HashMap<T, i32>
+    dist: Vec<i32>
 }
 
 pub fn main() {
-    let mut valves: HashMap<&str, Valve<&str>> = include_str!("../puzzles/16.txt").lines().map(|l|
-        (&l[6..8], Valve {
-            name: &l[6..8],
+    let input = include_str!("../puzzles/16.txt");
+    let lut: HashMap<&str, usize> = input.lines().map(|l| &l[6..8]).enumerate().map(|(i, n)| (n, i)).collect();
+
+    let mut valves: Vec<Valve> = input.lines().map(|l| {
+        let mut v = vec![i32::MAX / 2; lut.len()];
+        v[lut[&l[6..8]]] = 0;
+
+        Valve {
             flow_rate: l[23..].split_once(';').unwrap().0.parse().unwrap(),
             dist: if let Some(tunnel) = l.split_once("leads to valve ").unzip().1 {
-                HashMap::from([(&l[6..8], 0), (tunnel, 1)])
+                v[lut[tunnel]] = 1;
+                v
             } else {
-                let mut m: HashMap<&str, i32> = l.split_once("to valves ").unwrap().1.split(", ").map(|t| (t, 1)).collect();
-                m.insert(&l[6..8], 0);
-                m
-            },
-            ..Default::default()
-        })
-    ).collect();
-
-    let keys: Vec<&str> = valves.keys().map(|x| *x).collect();
-    for i in valves.values_mut() {
-        for j in keys.iter() {
-            i.dist.entry(j).or_insert(i32::MAX / 2);
+                for t in l.split_once("to valves ").unwrap().1.split(", ") {
+                    v[lut[t]] = 1;
+                }
+                v
+            }
         }
-    }
+    }).collect();
 
     // Floyd-warshall
-    for k in keys.iter() {
-        for i in keys.iter() {
-            for j in keys.iter() {
-                // tl/dr: fighting the borrow checker
-                let ik = *valves.get(i).unwrap().dist.get(k).unwrap();
-                let kj = *valves.get(k).unwrap().dist.get(j).unwrap();
-
-                let ij = valves.get_mut(i).unwrap().dist.get_mut(j).unwrap();
-                *ij = cmp::min(*ij, ik + kj);
+    for k in 0..valves.len() {
+        for i in 0..valves.len() {
+            for j in 0..valves.len() {
+                valves[i].dist[j] = cmp::min(valves[i].dist[j], valves[i].dist[k] + valves[k].dist[j]);
             }
         }
     }
 
-    let interesting_keys: Vec<&str> = valves.values().filter(|valve| valve.flow_rate > 0).map(|valve| valve.name).collect();
-    println!("Exercise 1: {}", trace("AA", 30, &valves, &mut interesting_keys.iter().map(|x| *x).collect(), false));
-    println!("Exercise 2: {}", trace("AA", 26, &valves, &mut interesting_keys.iter().map(|x| *x).collect(), true));
-    
+    let to_visit: u64 = valves.iter().enumerate().filter(|(_, v)| v.flow_rate == 0).map(|(i, _)| 1 << i).sum();
+    let mut trace_lut: HashMap<(usize, i32, u64, bool), i32> = HashMap::new();
+
+    println!("Exercise 1: {}", trace(lut["AA"], lut["AA"], 30, to_visit, &valves, &mut trace_lut, false));
+    println!("Exercise 2: {}", trace(lut["AA"], lut["AA"], 26, to_visit, &valves, &mut trace_lut, true));
 }
 
 
-fn trace<'a>(cur: &'a str, t: i32, valves: &HashMap<&str, Valve<&str>>, visit: &mut HashSet<&'a str>, split: bool) -> i32 {
-    let mut max_v = 0;
-    let valve = valves.get(cur).unwrap();
+fn trace(start: usize, cur: usize, t: i32, visit: u64, valves: &Vec<Valve>, lut: &mut HashMap<(usize, i32, u64, bool), i32>, elephant: bool) -> i32 {
+    if let Some(v) = lut.get(&(cur, t, visit, elephant)) {
+        return *v;
+    }
 
-    for v in visit.iter() {
-        if *valve.dist.get(v).unwrap() > t {
+    let mut max_v = 0;
+
+    for i in 0..valves.len() {
+        if (visit >> i) & 0b1 == 1 || valves[cur].dist[i] > t {
             continue;
         }
 
-        let mut n = visit.clone();
-        n.remove(v);
-
-        let c = t - (valve.dist.get(v).unwrap() + 1);
-        max_v = cmp::max(max_v, c * valves.get(v).unwrap().flow_rate + trace(v, c, valves, &mut n, split))
+        let c = t - (valves[cur].dist[i] + 1);
+        max_v = cmp::max(max_v, c * valves[i].flow_rate + trace(start, i, c, visit + (1 << i), valves, lut, elephant));
     }
 
-    if split {
-        max_v = cmp::max(max_v, trace("AA", 26, valves, visit, false));
+    if elephant {
+        max_v = cmp::max(max_v, trace(start, start, 26, visit, valves, lut, false))
     }
 
+    lut.insert((cur, t, visit, elephant), max_v);
     max_v
 }
